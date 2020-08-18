89b9051b2786d8da3a8dd15ec75807ae05e71cfa
Lucas Alcântara de Souza <lucasalcantara18@gmail.com>
2020-08-17 22:24:11 -0300

    adicionando header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 import './pages/homepage/homepage.styles.scss';
@@ -12,6 +13,7 @@ import './pages/homepage/homepage.styles.scss';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
